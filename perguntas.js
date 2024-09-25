ae5452dd168c4542ed4aad601919fb04db040009
kwanza01 <161734821+kwanza01@users.noreply.github.com>
2024-09-25 11:19:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'lobotomy corportion',
+    'O que acontceu com carmen no final do jogo?',
+    'morreu... mas a sua criação acendeu a luz da esperança no distrito 21'
+)
+
+criaCartao(
+    'library of ruina',
+    'qum é a mulher que roland se refere em urban myth apos conversar com angela',
+    'ele comenta sobre angelica sua falecida esposa'
+)
+
+criaCartao(
+    'lobotomy corporation',
+    'oque é uma abnormalite',
+    'uma criatura que geralmnte se aposa de emoções ruins igual o Nothings there '
+)
+
+criaCartao(
+    'prayer for the loving sorrow',
+    'a quem roland se referia nesse poema',
+    'ele se referia a sua esposa falecida angelica e como oque tudo oq restou foi a dor e a tristeza'
+)
